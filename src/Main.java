bfe593b42a3d47aabfe5e79eef339902faf0f7f8
DavidAiko <kenx@fall.cat>
2025-02-13 17:37:49 -0500

    Actualización

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan David Ruge Garzón");
+        System.out.println("Vive en: Chía, Cundinamarca");
+    }
+}
